221001358b94aaf7911cb81cb40e48e46df3d252
Alexey Bolshakov <avbolshakov@gmail.com>
2023-06-26 18:35:22 +0300

    Первичный ключ для пользователя теперь УИД, добавил поля СНИЛС и ИНН

--- app/models.py
+++ app/models.py
@@ -1,17 +1,18 @@
 from app.extension import db
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Text
 from sqlalchemy.orm import relationship, backref
 from flask_login import UserMixin
-from sqlalchemy.dialects.postgresql import UUID
 import uuid
 
 
 class User(db.Model):
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    id = Column(Text(length=36), default=lambda: str(uuid.uuid4()), primary_key=True)
     first_name = Column(String(64))
     last_name = Column(String(64))
     surname = Column(String(64))
     email = Column(String(120), index=True, unique=True)
     password_hash = Column(String(128))
     last_seen = Column(DateTime, default=datetime.utcnow)
+    inn = Column(String(length=12))
+    snils = Column(String(length=11))
